9b9ef2b0ac80224b5bbd723597cc8fd230d2bbbf
Mauro-Pereira <mauropereira1096@gmail.com>
2021-04-06 21:00:10 -0300

    add remove and print functions

--- fila/fila.js
+++ fila/fila.js
@@ -5,6 +5,18 @@ const Queue = () =>{
         data.unshift(number);
     }
 
+    const remove = () =>{
+        if(data.length === 0){
+            return -1
+        }
+
+        const value = data[data.length-1];
+        data.splice(data.length-1,1)
+        return value
+    }
+
+    const print = () => console.log(data);
+
 
     return {
         add,
